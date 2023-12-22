1ae82cf3a71a79b90ac84052c142bf5aa9fdb9f2
Ronin.IO <jhowell713@gmail.com>
2023-12-22 03:40:58 -0500

    deck status label properly updates

--- scenes/interface/Hand.gd
+++ scenes/interface/Hand.gd
@@ -11,14 +11,17 @@ func _ready() -> void:
 	$CardSlot1.grab_focus()
 
 func add_card(card: MetalCardData) -> void:
-	var index = cards_in_hand.find(null)
-	if index < 0:
-		emit_signal("hand_full")
-		print("Hand is full.")
+	if is_full():
 		return
-	
+	var index = cards_in_hand.find(null)
+
 	var new_card = CardPrinter.instance_card(card_template, card)
 	cards_in_hand[index] = new_card
 	var slot = get_child(index)
 	slot.add_child(new_card)
 
+func is_full() -> bool:
+	var status = cards_in_hand.find(null) < 0
+	if status == true:
+		emit_signal("hand_full")
+	return status
