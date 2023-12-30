c9dae67f8621f5e9da196390ed32029e80ee5d64
Ronin.IO <jhowell713@gmail.com>
2023-12-30 03:53:16 -0500

    Added the pathfinding framework.

--- scenes/levels/nav test/Navigation.gd
+++ scenes/levels/nav test/Navigation.gd
@@ -8,39 +8,32 @@ extends Node
 var widget_state := "Idle"
 
 var block_grid := {}
+var navigation_points := {}
 
-
+var astar := AStar3D.new()
+var nav_marker := preload("res://scenes/levels/nav test/nav_marker.tscn")
 
 func _ready() -> void:
 	#init grid
 	_initialize_block_grid()
 
-	# Set control widget above player's head
+	# init astar
+	navigation_points = _initialize_astar()
+	
+	#DEBUG pathfind
+	var valid_path := astar.get_point_path(navigation_points[Vector3(1, 3, 3)], navigation_points[Vector3(11, 3, 3)])
+
+	for location in valid_path:
+		var node = nav_marker.instantiate()
+		node.position = location
+		get_tree().root.add_child.call_deferred(node)
+
+	# Parent widget to the player
 	control_widget.reparent(player)
-	control_widget.position = Vector3.ZERO + Vector3.UP * block_size 
+	control_widget.position = Vector3.ZERO #+ Vector3.UP * block_size 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		match widget_state:
-			"Idle":
-					var blocks_in_range := get_blocks_in_range(player) # Could use this for control_widget
-					for block in blocks_in_range:
-						activate_block(block)
-					widget_state = "Selection"
-
-			"Selection":
-					move_player_to(control_widget.global_position + Vector3.DOWN * block_size)
-					get_tree().call_group("Blocks", "hide_marker")
-					widget_state = "Idle"
-		
-	if event.is_action_pressed("ui_up") and widget_state == "Selection":
-		move_selector(Vector2.UP)
-	if event.is_action_pressed("ui_down") and widget_state == "Selection":
-		move_selector(Vector2.DOWN)
-	if event.is_action_pressed("ui_left") and widget_state == "Selection":
-		move_selector(Vector2.LEFT)
-	if event.is_action_pressed("ui_right") and widget_state == "Selection":
-		move_selector(Vector2.RIGHT)
+	pass
 
 # add second argument for eligible blocks
 ## Attempts to move selector over a valid block. Reminder that the selector is one block above player.		
@@ -57,7 +50,7 @@ func move_selector(direction: Vector2) -> void:
 ## Rewrite this fucking function
 func move_player_to(location: Vector3) -> void:
 	player.global_position = location
-	control_widget.position = Vector3.ZERO + Vector3.UP * block_size
+	control_widget.position = Vector3.ZERO #+ Vector3.UP * block_size
 
 
 
@@ -85,6 +78,7 @@ func _initialize_block_grid() -> void:
 func deactivate_block(block: GridBlock) -> void:
 	if block: block.hide_marker()
 
+#TODO the highlight should exist on the grid layer of the space you want to get to, not the block underneath
 ## Activates highlight of block. Does nothing if block cannot be found.
 func activate_block(block) -> void:
 	if block: block.show_marker()
@@ -110,7 +104,48 @@ func neighbor_west(block: Node3D):
 	var target_location := block.position + Vector3.LEFT * block_size
 	return block_grid[target_location] if block_grid.has(target_location) else null
 
-## Returns the block below (-x) of given block. If no block exists, returns null.
+## Returns the block below (-y) of given block. If no block exists, returns null.
 func neighbor_below(block: Node3D):
 	var target_location := block.position + Vector3.DOWN * block_size
 	return block_grid[target_location] if block_grid.has(target_location) else null
+
+## Returns the block above (+y) of given block. If no block exists, returns null.
+func neighbor_above(block: Node3D):
+	var target_location := block.position + Vector3.UP * block_size
+	return block_grid[target_location] if block_grid.has(target_location) else null
+
+
+### This needs careful planning.
+### Any empty space above a solid block should be added.
+### Add connections that correspond to 3D space
+### Verticality can come later, for now focus on single layer.
+
+## But also they need to be connected in a gridlike manner.
+
+## Generates nav points for A* pathfinding
+func _initialize_astar() -> Dictionary:
+	# Store valid locations assigned to their ID in the nav
+	var valid_points := {}
+
+	# Add valid locations to the nav
+	for location in block_grid:
+		# Find out of there's an empty space above the block
+		if !neighbor_above(block_grid[location]): # If yes, add the open space to the nav
+			var point_id: int = astar.get_available_point_id()
+			valid_points[location + Vector3.UP * block_size] = point_id
+			astar.add_point(point_id, location + Vector3.UP * block_size)
+
+	# Find valid connections
+	for location in valid_points:
+		var north: Vector3 = location + block_size * Vector3.FORWARD
+		var south: Vector3 = location + block_size * Vector3.BACK
+		var east: Vector3 = location + block_size * Vector3.RIGHT
+		var west: Vector3 = location + block_size * Vector3.LEFT
+		# validate adjacent candidates and then connect them
+		if valid_points.has(north): astar.connect_points(valid_points[location], valid_points[north])
+		if valid_points.has(south): astar.connect_points(valid_points[location], valid_points[south])
+		if valid_points.has(east): astar.connect_points(valid_points[location], valid_points[east])
+		if valid_points.has(west): astar.connect_points(valid_points[location], valid_points[west])
+
+	return valid_points
+
