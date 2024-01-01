d9b8696a4abf23a637195d8c2f28ee51b136fa34
Ronin.IO <jhowell713@gmail.com>
2023-12-31 21:51:59 -0500

    movement is now animated
    add interrupt functionality via event bus

--- agents/Agent.gd
+++ agents/Agent.gd
@@ -1,32 +1,38 @@
 ### All this shit is broken
 
 class_name Agent
-extends CharacterBody3D
+extends Node3D
 
-@export var move_speed := 2.5
+@export var movement_speed: float = 4
 
-var facing := Vector3.FORWARD
+var start_movement := false
 
 signal point_reached
 signal finished_movement
 
-var destination = null
+var _path: PackedVector3Array = [] : set = set_path
 
-func _physics_process(_delta: float) -> void:
-	if destination != null:
-		velocity = global_position.direction_to(destination) * move_speed
-		if global_position.distance_to(destination) <= 1:
-			destination = null
-			emit_signal("point_reached")
-	
-	move_and_slide()
+var facing_direction := Vector3.FORWARD
 
-## Move pawn through a given path
-func move_on_path(path: PackedVector3Array) -> void:
-	for point in path:
-		destination = point
-		await point_reached
-	
-	print("bop")
-	
-	emit_signal("finished_movement")
+func _physics_process(delta: float) -> void:
+	if start_movement:
+		move_along_path(position, delta)
+
+func move_along_path(origin: Vector3, delta: float) -> void:
+	# Check if agent is at the next location
+	if !position == _path[0]:
+		position = origin.move_toward(_path[0], delta * movement_speed)
+	else:
+		var checkpoint = _path[0]
+		_path.remove_at(0)
+		emit_signal("point_reached")
+		if _path.is_empty(): # If path is empty, finish operation
+			emit_signal("finished_movement")
+			start_movement = false
+			return
+		else: # reset start point then loop back
+			move_along_path(checkpoint, delta)
+
+func set_path(arg: PackedVector3Array) -> void:
+	_path = arg
+	start_movement = true
--- scenes/levels/nav test/ControlTest.gd
+++ scenes/levels/nav test/ControlTest.gd
@@ -9,9 +9,9 @@ func _input(event):
 	if !has_control:
 		return
 	if event.is_action_pressed("ui_accept"):
-		get_viewport().set_input_as_handled()
+		get_viewport().set_input_as_handled() # Why does this work
 		has_control = false		
-		var path = await navigation.process_movement(player.global_position, 3)
-		player.move_on_path(path)
+		var path := await navigation.process_movement(player.global_position, 3)
+		player.set_path(path)
 		await player.finished_movement
 		has_control = true
