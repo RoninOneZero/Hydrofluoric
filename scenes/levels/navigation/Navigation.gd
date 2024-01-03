ce2ca184ccd87d009009df032db25f24ccd0e47a
Ronin.IO <jhowell713@gmail.com>
2024-01-03 00:32:49 -0500

    stage setting for new astar configuration

--- scenes/levels/navigation/Navigation.gd
+++ scenes/levels/navigation/Navigation.gd
@@ -24,12 +24,7 @@ func _ready() -> void:
 
 	
 func initialize_navigation(blocks: Array[Node3D], ramps: Array[Node3D]) -> void:
-	for block in blocks:
-		block_grid[block.global_position] = block
-	# for location in ramps:
-	# 	ramp_grid[location.global_position] = location
-
-	navigation_points = _initialize_astar()	
+	pass
 
 
 
@@ -121,60 +116,6 @@ func get_points_in_range(location: Vector3, distance: int) -> PackedVector3Array
 	return point_list
 
 ## Generates nav points for A* pathfinding
-func _initialize_astar() -> Dictionary:
-	# Store valid locations assigned to their ID in the nav
-	var valid_points := {}
-
-	# Add valid locations to the nav
-	for location in block_grid:
-		# Find out of there's an empty space above the block
-		if !neighbor_above(block_grid[location]): # If yes, add the open space to the nav
-			var point_id: int = astar.get_available_point_id()
-			valid_points[location + Vector3.UP * block_size] = point_id
-			astar.add_point(point_id, location + Vector3.UP * block_size) # is *block_size needed?
-
-	# Find valid connections
-	for location in valid_points:
-		var north: Vector3 = location + block_size * Vector3.FORWARD
-		var south: Vector3 = location + block_size * Vector3.BACK
-		var east: Vector3 = location + block_size * Vector3.RIGHT
-		var west: Vector3 = location + block_size * Vector3.LEFT
-		# validate adjacent candidates and then connect them
-		if valid_points.has(north): astar.connect_points(valid_points[location], valid_points[north])
-		if valid_points.has(south): astar.connect_points(valid_points[location], valid_points[south])
-		if valid_points.has(east): astar.connect_points(valid_points[location], valid_points[east])
-		if valid_points.has(west): astar.connect_points(valid_points[location], valid_points[west])
-
-
-	for location in ramp_grid:
-		# Remove the nav_point of any block sharing this space
-		var target_block: Vector3 = location + Vector3(0, +0.5, 0)
-		if valid_points.has(target_block):
-			astar.remove_point(valid_points[target_block])
-			valid_points.erase(target_block)
-		target_block = location + Vector3(0, -0.5, 0)
-		if valid_points.has(target_block):
-			astar.remove_point(valid_points[target_block])
-			valid_points.erase(target_block)
-		
-		# Find out if there's empty space above the ramp
-		if !block_grid.has(ramp_grid[location].position + Vector3.UP * 1.5):
-			var point_id: int = astar.get_available_point_id()
-			var nav_position: Vector3 = location + Vector3.UP * block_size
-			valid_points[nav_position] = point_id
-			astar.add_point(point_id, nav_position)
-
-			#Establish the resulting navpoint's connections
-			#look at potential lower ramp locations
-			# if no ramp, connect to possible block
-			# look at potential upp ramp locations
-			# if no ramp connect to possible block
-		
-
-
-		
-		
-
-
-	return valid_points
+func _initialize_astar():
+	pass
 
--- scenes/levels/test_level/MissionController.gd
+++ scenes/levels/test_level/MissionController.gd
@@ -6,16 +6,4 @@ extends Node
 @export var geometry: GeometryController
 
 func _ready() -> void:
-	# Initialize components
-	var block_list = geometry.get_block_list()
-	var ramp_list = geometry.get_ramp_list()
-
-	for block in block_list:
-		navigation.add_child(block)
-
-	for ramp in ramp_list:
-		navigation.add_child(ramp)
-
-	
-	navigation.initialize_navigation(block_list, ramp_list)
-
+	pass
