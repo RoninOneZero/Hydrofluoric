5f84ce81fa74444787a51d2d66b65e56a5e5eb16
Ronin.IO <jhowell713@gmail.com>
2024-01-12 00:36:30 -0500

    frustration

--- scenes/levels/test_level/DeckHandler.gd
+++ scenes/levels/test_level/DeckHandler.gd
@@ -1,16 +1,21 @@
+###	Hold the deck list as an array of card datas
+### a card object should be a class object based off the card data, and hold a ref to the data
+### a card UI object should be "stupid" and hold no data of its own.
+
 class_name DeckHandler
 extends Control
 
+@export var enabled: = false
 @export var deck: Array[MetalCardData]
 
 @onready var inspector: = $Inspector
 @onready var hand: = $Hand
 
+var hand_limit: = 6
 
 var card_template := preload("res://scenes/cards forms/metal card/metal_card.tscn")
-var cards_in_hand: Array[MetalCard] = []
+var cards_in_hand: Array[MetalCard] = [] 
 
-var enabled: = false
 
 func _ready() -> void:
 
@@ -18,8 +23,8 @@ func _ready() -> void:
 	for card in deck:
 		var card_node: = card_template.instantiate()
 		card_node.update_card(card)
-		hand.add_child(card_node)
-		print(card_node.name)
+		place_card_in_hand(card_node)
+		
 
 func _unhandled_input(event: InputEvent) -> void:
 	if !enabled:
@@ -28,3 +33,17 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_accept"):
 		get_viewport().set_input_as_handled()
 		print("Dech Handler received confirm input.")
+
+func place_card_in_hand(card:Control) -> void:
+	if cards_in_hand.size() >= hand_limit:
+		return
+
+	cards_in_hand.append(card)	
+	hand.add_child(card)
+
+# Maybe this should return the amount of cards drawn?
+func draw_to_limit() -> void:
+	var difference = hand_limit - cards_in_hand.size()
+	if difference > 0:
+		for i in difference:
+			place_card_in_hand(deck.pop_front())
